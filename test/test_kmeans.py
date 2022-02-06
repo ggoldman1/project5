7df7e6a89b4aaf1219d9aaf58db6d7873741a5f1
Grant Goldman <grant.goldman@ucsf.edu>
2022-02-06 13:49:59 -0800

    fix issue with pytest not finding package, need empyt __init__.py file

--- test/test_kmeans.py
+++ test/test_kmeans.py
@@ -1,11 +1,11 @@
-from kmeans import KMeans
+import cluster
 import pytest
 
 def test_kmeans():
     """
     Various tests of my kmeans implementation
     """
-    data, _ = make_clusters(n=5)
-    km = KMeans(n=10)
+    data, _ = cluster.utils.make_clusters(n=5)
+    km = cluster.kmeans.KMeans(k=10)
     with pytest.raises(ValueError):
         km.fit(data)
